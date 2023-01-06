a5b07c429e52e769dffe086a1f933e3528b49f31
JosueDavid21 <josue_b_21@outlook.com>
2023-01-05 23:25:20 -0500

    Culminado todos los items

---
+++ execute.bat
@@ -0,0 +1,4 @@
+start java -Dserver.port=7001 -classpath build/install/ex01BolanosJosuelib/* com.distribuida.Servidor
+start java -Dserver.port=8001 -classpath build/install/ex01BolanosJosuelib/* com.distribuida.Servidor
+start java -Dserver.port=9001 -classpath build/install/ex01BolanosJosuelib/* com.distribuida.Servidor
+start java -Dserver.port=9191 -classpath build/install/ex01BolanosJosuelib/* com.distribuida.Servidor
